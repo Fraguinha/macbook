7040258435abfd5d7398d6021a85e5d7364458c8
João Fraga <joaoluisfreirefraga@gmail.com>
2021-03-18 18:00:13 +0000

    Thu Mar 18 18:00:13 WET 2021

---
+++ statusbar/xtz.15s.sh
@@ -0,0 +1,42 @@
+# Configurations
+# <swiftbar.hideSwiftBar>true</swiftbar.hideSwiftBar>
+# <swiftbar.hideRunInTerminal>true</swiftbar.hideRunInTerminal>
+# <swiftbar.hideDisablePlugin>true</swiftbar.hideDisablePlugin>
+# <swiftbar.hideAbout>true</swiftbar.hideAbout>
+
+# Addresses
+WALLET_ADDRESS='tz1VjYmQHw4QE6tYTQLLfWNjqkUNwMC7YPoF'
+MINTER_ADDRESS='KT1Ty2uAmF5JxWyeGrVpk17MEyzVB8cXs8aJ'
+OVEN_ADDRESS='KT1AKcRY2F8uZv5psx2gUm7AoTk8Cp6ei4hM'
+
+# Tezos
+PRICE=$(curl -sX GET 'https://api.coingecko.com/api/v3/simple/price?ids=tezos&vs_currencies=eur,usd')
+
+EUR=$(echo $PRICE | jq --raw-output '.tezos.eur')
+USD=$(echo $PRICE | jq --raw-output '.tezos.usd')
+
+# Wallet
+WALLET=$(curl -sX GET "https://api.tzstats.com/explorer/account/$WALLET_ADDRESS")
+
+XTZ=$(echo $WALLET | jq --raw-output '.total_balance')
+
+# Kolibri
+OVEN=$(curl -sX GET "https://api.tzstats.com/explorer/account/$OVEN_ADDRESS")
+OVEN_STORAGE=$(curl -sX GET "https://api.tzstats.com/explorer/contract/$OVEN_ADDRESS/storage")
+MINTER_STORAGE=$(curl -sX GET "https://api.tzstats.com/explorer/contract/$MINTER_ADDRESS/storage")
+
+OVEN=$(echo $OVEN | jq --raw-output '.total_balance')
+KUSD=$(echo $OVEN_STORAGE | jq --raw-output '.value.borrowedTokens')
+INTEREST_RATE=$(echo $MINTER_STORAGE | jq --raw-output '.value.interestIndex')
+
+# Status
+python3 -c "print(f'ꜩ : {$EUR:,.2f} €')"
+echo "---"
+echo "Wallet"
+python3 -c "print(f'Personal : {$XTZ:,.2f} ꜩ | href=\'https://tzkt.io/tz1VjYmQHw4QE6tYTQLLfWNjqkUNwMC7YPoF/rewards\'')"
+echo "---"
+echo "Kolibri"
+python3 -c "print(f'Oven : {$OVEN:,.2f} ꜩ | href=\'https://tzkt.io/KT1AKcRY2F8uZv5psx2gUm7AoTk8Cp6ei4hM/rewards\'')"
+python3 -c "print(f'Borrowed : {$KUSD/10**18:,.2f} $')"
+python3 -c "print(f'Stability fee : {((1*(1+.$INTEREST_RATE/513000)**(513000))-1)*100:.2f} %')"
+python3 -c "print(f'Collateral use : {((($KUSD)/10**18)/(($OVEN*$USD)/2))*100:,.2f} %')"
