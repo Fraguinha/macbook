47aa076e23b88b8f317070db512e231a2b91557c
Fraguinha <joaoluisfreirefraga@gmail.com>
2021-07-02 18:00:10 +0100

    Fri Jul  2 18:00:10 WEST 2021

--- setup/appstore.sh
+++ setup/appstore.sh
@@ -1,10 +1,13 @@
-mas install 1295203466 # Microsoft Remote Desktop
-mas install 1352778147 # Bitwarden
-mas install 1511962480 # Galleon
-mas install 937984704 # Amphetamine
 mas install 1440147259 # AdGuard for Safari
 mas install 1444383602 # GoodNotes
 mas install 497799835 # Xcode
-mas install 803453959 # Slack
 mas install 441258766 # Magnet
+mas install 409183694 # Keynote
+mas install 1295203466 # Microsoft Remote Desktop
+mas install 1511962480 # Galleon
+mas install 803453959 # Slack
+mas install 409201541 # Pages
 mas install 747648890 # Telegram
+mas install 1352778147 # Bitwarden
+mas install 937984704 # Amphetamine
+mas install 409203825 # Numbers
