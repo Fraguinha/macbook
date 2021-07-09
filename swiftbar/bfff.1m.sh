a687640caad803fe5c191585740e93b1a2211ad6
Fraguinha <joaoluisfreirefraga@gmail.com>
2021-07-09 03:13:30 +0100

    Remove swiftbar

--- swiftbar/bfff.1m.sh
+++
@@ -1,8 +0,0 @@
-# Configurations
-# <swiftbar.hideSwiftBar>true</swiftbar.hideSwiftBar>
-# <swiftbar.hideRunInTerminal>true</swiftbar.hideRunInTerminal>
-# <swiftbar.hideDisablePlugin>true</swiftbar.hideDisablePlugin>
-# <swiftbar.hideAbout>true</swiftbar.hideAbout>
-
-# Status
-python3 -c "import datetime;print(f'SSSA : {(datetime.datetime(2021, 9, 12) - datetime.datetime.now()).days} days')"
--- swiftbar/xtz.5s.sh
+++
@@ -1,7 +0,0 @@
-# Configurations
-# <swiftbar.hideSwiftBar>true</swiftbar.hideSwiftBar>
-# <swiftbar.hideRunInTerminal>true</swiftbar.hideRunInTerminal>
-# <swiftbar.hideDisablePlugin>true</swiftbar.hideDisablePlugin>
-# <swiftbar.hideAbout>true</swiftbar.hideAbout>
-
-echo "$(xtz)"
